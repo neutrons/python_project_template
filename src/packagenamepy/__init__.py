10f31e6ce7ada294bc07dad62ab77e00f2a8c745
Kevin A. Tactac <tactacka@ornl.gov>
2024-02-19 13:57:14 -0500

    add try/except

--- src/packagenamepy/__init__.py
+++ src/packagenamepy/__init__.py
@@ -2,8 +2,11 @@
 Contains the entry point for the application
 """
 
-from ._version import __version__  # noqa: F401
-
+try:
+    from ._version import __version__  # noqa: F401
+except ImportError:
+    __version__ = "unknown"
+    
 
 def PackageName():  # pylint: disable=invalid-name
     """This is needed for backward compatibility because mantid workbench does "from shiver import Shiver" """
