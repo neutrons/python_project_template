c68aab209497608c052f925fcdcb6b28b196738e
Pete Peterson <petersonpf@ornl.gov>
2026-09-18 14:19:46 -0400

    Rewrite the version test so it doesn't need to be edited

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,9 +1,17 @@
+import pytest
+from packaging import version as py_version
+
 from packagenamepy import __version__
 
 
 def test_version():
-    assert __version__ == "unknown" or "dev" in __version__ or "0.1.0" in __version__
-    #      ^ conda env will default to "unknown" if not set
-    #                                  ^ pixi will default to the default tag in pryproject.toml
-    #                                    if not set, it will default to "0.0.1" + "devxxx" on local dev builds
-    #                                                          ^ this is the default version on runner
+    assert __version__, "empty version"
+    # conda env will default to "unknown" if not set
+    # pixi will default to the default tag in pryproject.toml
+    if __version__ != "unknown":
+        try:
+            version = py_version.parse(__version__)
+            assert version.major >= 0
+            assert version.minor >= 0
+        except py_version.InvalidVersion:
+            pytest.fail(f"Failed to parse version {__version__}")
